f17405540dc90ff960b2d8f01a36d92a3e85fee7
hmouis <houdayfamouis@gmail.com>
2025-10-02 11:20:40 +0100

    FIX: error in upper case and ADD: flags to Makefile

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -2,35 +2,18 @@
 
 int main(int ac, char **av)
 {
-    int i, j;
     char c;
 
-    i = 0;
-    j = 1;
     if (ac == 1)
     {
         std::cout<< "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" ;
         return 0;
     }
-    else
+    for(int j = 1; av[j]; j++)
     {
-        while (av[j])
-        {
-            i = 0;
-            while (av[j][i])
-            {
-                if (isalpha(av[j][i]))
-                {
-                    c = av[j][i] - 32 ;
-                    std::cout<< c ;
-                }
-                else
-                    std::cout<< av[j][i];
-                i++;
-            }
-            j++;
-        }
-    }   
+	    for(int i = 0; av[j][i]; i++)
+		    std::cout << (char )toupper(av[j][i]);
+    }
     std::cout<< "\n";
     return 0;
 }
