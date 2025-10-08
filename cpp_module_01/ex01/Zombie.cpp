434fd9eef7cb58461d910f9d6a78f9330b052928
hmouis <houdayfamouis@gmail.com>
2025-10-08 12:33:20 +0100

    ADD: ex01 C01

---
+++ cpp_module_01/ex01/Zombie.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmouis <hmouis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 11:39:20 by hmouis            #+#    #+#             */
+/*   Updated: 2025/10/05 15:11:03 by hmouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie() 
+{};
+
+Zombie::Zombie(std::string name)
+{
+    this->name = name;
+}
+
+void Zombie::announce( void )
+{
+    if (name == "Foo")
+        std::cout << "Foo: ";
+    else
+        std::cout << "<" << name << ">: ";
+    std::cout << "BraiiiiiiinnnzzzZ...\n";
+}
+
+std::string Zombie::getName()
+{
+    return (name);
+}
+Zombie::~Zombie()
+{
+    std::cout << "the zombie: " << getName() << " has been destroyed.\n";
+}
+
---
+++ cpp_module_01/ex01/zombieHorde.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmouis <hmouis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 14:33:30 by hmouis            #+#    #+#             */
+/*   Updated: 2025/10/05 15:18:42 by hmouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void Zombie::setName(std::string name)
+{
+    this->name = name;
+}
+
+Zombie* zombieHorde( int N, std::string name )
+{
+    Zombie *zombie_arr = new Zombie[N];
+    
+    for (int i = 0; i < N; i++)
+    {
+        zombie_arr[i].setName(name);
+        zombie_arr[i].announce();
+    }
+    return (zombie_arr);
+}
