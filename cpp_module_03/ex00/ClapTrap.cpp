d3aa147699d864114e049dde3c8c4651f4a8ba99
hmouis <houdayfamouis@gmail.com>
2025-11-18 18:11:03 +0100

    add CPP01 ex00

--- cpp_module_03/ex00/ClapTrap.cpp
+++ cpp_module_03/ex00/ClapTrap.cpp
@@ -29,4 +29,55 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& other)
     return *this;
 }
 
+void ClapTrap::setHitPoints(const unsigned int& HitPoints) {this->HitPoints = HitPoints; };
+void ClapTrap::setEnergyPoints(const unsigned int& EnergyPoints) {this->EnergyPoints = EnergyPoints; };
+void ClapTrap::setAttackDamage(const unsigned int& AttackDamage) {this->AttackDamage = AttackDamage; };
+        
+unsigned int ClapTrap::getHitPoints() { return HitPoints; };
+unsigned int ClapTrap::getEnergyPoints(){ return EnergyPoints; };
+unsigned int ClapTrap::getAttackDamage(){ return AttackDamage; };
+std::string ClapTrap::getName(){ return Name; };
+
+void ClapTrap::attack(const std::string& target)
+{
+    if (getHitPoints() == 0)
+        std::cout << "It's dead\n";
+    else if (getEnergyPoints() > 0)
+    {
+        setEnergyPoints(getEnergyPoints() - 1);
+        std::cout << "ClapTrap " << this->Name << " attacks " << target <<", causing " <<
+            getAttackDamage() << " points of damage!" << std::endl;
+    }
+    else
+        std::cout << "ClapTrap " << getName() << " Can't attack " << target << " No EnergyPoints left\n";
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if (getHitPoints() > 0)
+    {
+        if (amount > getHitPoints())
+            setHitPoints(0);
+        else
+            setHitPoints(getHitPoints() - amount);
+        std::cout << "ClapTrap " << getName() << " Take " << amount << " points of damage!\n";
+    }
+    else 
+        std::cout << "ClapTrap " << getName() << " Can't take damage because it's dead\n";
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (getHitPoints() == 0)
+        std::cout << "ClapTrap " << getName() << " Can't repair because it have 0 HitPoints\n";
+    else if (getEnergyPoints() == 0)
+        std::cout << "ClapTrap " << getName() << " Can't repair because it have 0 EnergyPoints\n";
+    else
+    {
+        setEnergyPoints(getEnergyPoints() - 1);
+        setHitPoints(getHitPoints() + amount);
+        std::cout << "ClapTrap " << getName() << " regain " << amount << " of points\n";
+    }
+}
+
 ClapTrap::~ClapTrap(){ std::cout << "Destructor called\n"; }
--- cpp_module_03/ex00/ClapTrap.hpp
+++ cpp_module_03/ex00/ClapTrap.hpp
@@ -7,16 +7,26 @@ class ClapTrap
 {
     private:
         std::string Name;
-        int HitPoints;
-        int EnergyPoints;
-        int AttackDamage;
+        unsigned int HitPoints;
+        unsigned int EnergyPoints;
+        unsigned int AttackDamage;
     public:
         ClapTrap();
-        ClapTrap::ClapTrap(std::string Name);
+        ClapTrap(std::string Name);
         ClapTrap(const ClapTrap& other);
         ClapTrap& operator=(const ClapTrap& other);
         ~ClapTrap();
         
+        void setHitPoints(const unsigned int& HitPoints);
+        void setEnergyPoints(const unsigned int& EnergyPoints);
+        void setAttackDamage(const unsigned int& AttackDamage);
+        
+        std::string getName();
+        unsigned int getHitPoints();
+        unsigned int getEnergyPoints();
+        unsigned int getAttackDamage();
+        
+
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
